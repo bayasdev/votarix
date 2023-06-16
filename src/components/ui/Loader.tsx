5f2e0b07f17cc3c1894c147cfca252ef35e6c2cc
Victor Bayas <victorsbayas@gmail.com>
2023-06-16 16:47:09 -0500

    add loader

---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="grid h-screen place-content-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+};
+
+export default Loader;
