b69b625193d6d9514c3a1f27eb3e6d5cafd06740
Victor Bayas <victorsbayas@gmail.com>
2023-07-17 17:48:56 -0500

    add trusted domains

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    domains: ['cdn.pixabay.com', 'images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
