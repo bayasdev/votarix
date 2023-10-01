5bb7eb5ba1184740e4ccb4f1bbe25369f11da9f1
Victor Bayas <victorsbayas@gmail.com>
2023-10-01 17:40:04 -0500

    disable dni validation for candidates

--- src/lib/validators/candidate.ts
+++ src/lib/validators/candidate.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import validateDni from '../helpers/validateDni';
+// import validateDni from '../helpers/validateDni';
 
 export const CandidateValidator = z.object({
   name: z.string().min(1, 'El campo es requerido'),
@@ -10,9 +10,9 @@ export const CandidateValidator = z.object({
     .or(z.literal('')),
   document: z
     .string()
-    .refine((value) => validateDni(value || ''), {
-      message: 'El número de cédula ingresado no es válido',
-    })
+    // .refine((value) => validateDni(value || ''), {
+    //   message: 'El número de cédula ingresado no es válido',
+    // })
     .optional()
     .or(z.literal('')),
   partyId: z.string().min(1, 'El campo es requerido'),
