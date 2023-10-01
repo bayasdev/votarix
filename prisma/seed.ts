e350021ca139f918efd50f7dd367dd6546e262fa
Victor Bayas <victorsbayas@gmail.com>
2023-10-01 17:39:34 -0500

    update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient, Role } from '@prisma/client';
 import { hash } from 'bcrypt';
+import dayjs from 'dayjs';
 
 const prisma = new PrismaClient();
 async function main() {
@@ -15,6 +16,23 @@ async function main() {
     },
   });
 
+  const election1 = await prisma.election.create({
+    data: {
+      name: 'Elecciones 1',
+      description:
+        'Elecciones de representantes estudiantiles al honorable consejo universitario',
+      startTime: new Date(),
+      endTime: dayjs(new Date()).add(12, 'hour').toDate(),
+    },
+  });
+
+  const position1 = await prisma.position.create({
+    data: {
+      name: 'Presidente',
+      electionId: election1.id,
+    },
+  });
+
   const party1 = await prisma.party.create({
     data: {
       name: 'Partido 1',
@@ -27,17 +45,37 @@ async function main() {
     },
   });
 
-  const candidate = await prisma.candidate.create({
+  const candidate1 = await prisma.candidate.create({
     data: {
       name: 'Juan Pérez',
       document: '1799999999',
       email: 'jperez@ejemplo.com',
       proposals: 'Generaré 10000 nuevos empleos en el área de STEM',
       partyId: party1.id,
+      positionId: position1.id,
     },
   });
 
-  console.log({ admin, party1, party2, candidate });
+  const candidate2 = await prisma.candidate.create({
+    data: {
+      name: 'María González',
+      document: '1799999998',
+      email: 'mgonzales@bayas.dev',
+      proposals: 'Mejoraré el transporte público',
+      partyId: party2.id,
+      positionId: position1.id,
+    },
+  });
+
+  console.log({
+    admin,
+    election1,
+    position1,
+    party1,
+    party2,
+    candidate1,
+    candidate2,
+  });
 }
 main()
   .then(async () => {
