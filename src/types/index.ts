3ea3fb4c8075e1a80869f857bbe93d6ff170b4f7
Victor Bayas <victorsbayas@gmail.com>
2023-10-02 20:51:30 -0500

    add type utilities and simplify code

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,32 @@
 import { Candidate, Election, Party, Position, User } from '@prisma/client';
 
-export type SafeUser = Omit<
-  User,
-  'createdAt' | 'updatedAt' | 'emailVerified'
-> & {
-  emailVerified: string | null;
+type Safe<T> = Omit<T, 'createdAt' | 'updatedAt'> & {
   createdAt: string;
   updatedAt: string;
 };
 
-export type SafeParty = Omit<Party, 'createdAt' | 'updatedAt'> & {
-  createdAt: string;
-  updatedAt: string;
+type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
+
+type NonNullable<T> = T extends null | undefined ? never : T;
+
+export type SafeUser = Without<Safe<User>, 'emailVerified'> & {
+  emailVerified: NonNullable<string>;
 };
 
-export type SafePosition = Omit<Position, 'createdAt' | 'updatedAt'> & {
-  createdAt: string;
-  updatedAt: string;
-};
-
-export type SafeCandidate = Omit<Candidate, 'createdAt' | 'updatedAt'> & {
-  createdAt: string;
-  updatedAt: string;
-};
-
-export type SafeElection = Omit<
-  Election,
-  'startTime' | 'endTime' | 'createdAt' | 'updatedAt'
-> & {
+export type SafeElection = Without<Safe<Election>, 'startTime' | 'endTime'> & {
   startTime: string;
   endTime: string;
-  createdAt: string;
-  updatedAt: string;
+};
+export type SafePosition = Safe<Position>;
+
+export type SafePositionWithElection = Without<SafePosition, 'election'> & {
+  election?: SafeElection;
+};
+
+export type SafeParty = Safe<Party>;
+
+export type SafeCandidate = Safe<Candidate>;
+
+export type SafeCandidateWithParty = Without<SafeCandidate, 'party'> & {
+  party?: SafeParty;
 };
