77688ca0d723c327392a9db2c820843f502771af
Victor Bayas <victorsbayas@gmail.com>
2023-10-19 12:17:21 -0500

    bump

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,8 +2,8 @@ import { SiteConfig } from '@/types';
 
 export const siteConfig: SiteConfig = {
   name: 'Votarix',
-  description: 'Votarix E-Voting System (nextgen)',
-  version: '0.1.0 (nextgen)',
+  description: 'Votarix E-Voting System',
+  version: '1.0.0',
   url: 'https://tx.shadcn.com',
   ogImage: 'https://tx.shadcn.com/og.jpg',
   links: {
