34074de03c2c47deff12bd6a4d63dc11b3bd8a58
Victor Bayas <victorsbayas@gmail.com>
2023-10-19 16:21:07 -0500

    update signup form

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -3,25 +3,28 @@
 import * as React from 'react';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 
 import { cn } from '@/lib/utils';
 import { SignupRequest, SignupValidator } from '@/lib/validators/auth';
-import { buttonVariants } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+  FormControl,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 import { toast } from '@/components/ui/use-toast';
 import { Icons } from '@/components/icons';
 
 interface SignupFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function SignupForm({ className, ...props }: SignupFormProps) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignupRequest>({
+  const form = useForm<SignupRequest>({
     resolver: zodResolver(SignupValidator),
     defaultValues: {
       name: '',
@@ -33,7 +36,7 @@ export function SignupForm({ className, ...props }: SignupFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const router = useRouter();
 
-  function onSubmit(data: SignupRequest) {
+  const onSubmit: SubmitHandler<SignupRequest> = (data) => {
     axios
       .post('/api/signup', data)
       .then((response) => {
@@ -51,89 +54,98 @@ export function SignupForm({ className, ...props }: SignupFormProps) {
       .finally(() => {
         setIsLoading(false);
       });
-  }
+  };
 
   return (
     <div className={cn('grid gap-6', className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-6">
-          <div className="grid gap-2 md:grid-cols-2">
-            <div className="grid gap-2">
-              <Label htmlFor="name">Nombre</Label>
-              <Input
-                id="name"
-                placeholder="Juan Pérez"
-                type="text"
-                autoCorrect="off"
-                disabled={isLoading}
-                {...register('name')}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid gap-6">
+            <div className="grid gap-2 md:grid-cols-2">
+              <FormField
+                name="name"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="name"
+                        placeholder="Juan Pérez"
+                        disabled={isLoading}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-              {errors?.name && (
-                <p className="px-1 text-xs text-red-600">
-                  {errors.name.message}
-                </p>
-              )}
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="document">Número de Cédula</Label>
-              <Input
-                id="document"
-                placeholder="1700000000"
-                type="text"
-                autoCorrect="off"
-                disabled={isLoading}
-                {...register('document')}
+              <FormField
+                name="document"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Número de Cédula</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="document"
+                        placeholder="1700000000"
+                        disabled={isLoading}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-              {errors?.document && (
-                <p className="px-1 text-xs text-red-600">
-                  {errors.document.message}
-                </p>
-              )}
             </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Correo Electrónico</Label>
-            <Input
-              id="email"
-              placeholder="usuario@ejemplo.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-              {...register('email')}
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Correo Electrónico</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="email"
+                      placeholder="usuario@ejemplo.com"
+                      type="email"
+                      disabled={isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            {errors?.email && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Contraseña</Label>
-            <Input
-              id="password"
-              placeholder="••••••••"
-              type="password"
-              autoCapitalize="none"
-              autoCorrect="off"
-              disabled={isLoading}
-              {...register('password')}
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contraseña</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="password"
+                      placeholder="••••••••"
+                      type="password"
+                      disabled={isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            {errors?.password && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.password.message}
-              </p>
-            )}
+            <Button type="submit" disabled={isLoading}>
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Iniciar Sesión
+            </Button>
           </div>
-          <button className={cn(buttonVariants())} disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Iniciar Sesión
-          </button>
-        </div>
-      </form>
+        </form>
+      </Form>
     </div>
   );
 }
