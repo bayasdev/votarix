e420a2229f9737f95c4badf11d240605b4dfcf6f
Victor Bayas <victorsbayas@gmail.com>
2023-10-19 13:52:10 -0500

    make auth dynamic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,6 +11,8 @@ export const metadata: Metadata = {
   title: 'Iniciar sesión',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
