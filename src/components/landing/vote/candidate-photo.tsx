6be04dcc125d3d4dc2e823c976caf202ad93d3eb
Victor Bayas <victorsbayas@gmail.com>
2023-10-25 07:23:36 -0500

    update interface

--- src/components/landing/vote/candidate-photo.tsx
+++ src/components/landing/vote/candidate-photo.tsx
@@ -2,20 +2,20 @@ import { AvatarProps } from '@radix-ui/react-avatar';
 
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { Icons } from '@/components/icons';
+import { ElectionResultsCandidate } from '@/types';
 
 interface CandidatePhotoProps extends AvatarProps {
-  name: string;
-  imageUrl?: string | null;
+  candidate: ElectionResultsCandidate;
 }
 
-export function CandidatePhoto({ name, imageUrl }: CandidatePhotoProps) {
+export function CandidatePhoto({ candidate }: CandidatePhotoProps) {
   return (
     <Avatar className="h-32 w-32">
-      {imageUrl ? (
-        <AvatarImage alt={name} src={imageUrl} />
+      {candidate?.imageUrl ? (
+        <AvatarImage alt={candidate.name} src={candidate.imageUrl} />
       ) : (
         <AvatarFallback>
-          <span className="sr-only">{name}</span>
+          <span className="sr-only">{candidate.name}</span>
           <Icons.user className="h-16 w-16" />
         </AvatarFallback>
       )}
