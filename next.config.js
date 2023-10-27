2d0b4e131f9976b1a7bf4428595c438a03fc2217
Victor Bayas <victorsbayas@gmail.com>
2023-10-27 10:42:16 -0500

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ['utfs.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
