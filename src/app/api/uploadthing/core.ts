35357b05b243cdd9cc87dbcf55038ab268b63aa0
Victor Bayas <victorsbayas@gmail.com>
2023-10-31 18:24:27 -0500

    add middleware to ut

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,8 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import { UTApi } from 'uploadthing/server';
 
+import getCurrentUser from '@/actions/getCurrentUser';
+
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
@@ -8,10 +10,30 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   candidates: f({
     image: { maxFileSize: '2MB', maxFileCount: 1 },
-  }).onUploadComplete(() => {}),
+  })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+
+      if (!currentUser || currentUser.role !== 'ADMIN') {
+        throw new Error('No autorizado');
+      }
+
+      return { userId: currentUser.id };
+    })
+    .onUploadComplete(() => {}),
   parties: f({
     image: { maxFileSize: '2MB', maxFileCount: 1 },
-  }).onUploadComplete(() => {}),
+  })
+    .middleware(async () => {
+      const currentUser = await getCurrentUser();
+
+      if (!currentUser || currentUser.role !== 'ADMIN') {
+        throw new Error('No autorizado');
+      }
+
+      return { userId: currentUser.id };
+    })
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
