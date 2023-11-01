cff4c17a783e4725825d1dcd28bf84ac7521a9d2
Victor Bayas <victorsbayas@gmail.com>
2023-11-01 15:47:47 -0500

    update dashboard home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from 'next';
-import { Suspense } from 'react';
 
 import { getDashboardData } from '@/actions/dashboard';
+import Heading from '@/components/heading';
 import DashboardClient from '@/components/dashboard/home/client';
-import DashboardSkeleton from '@/components/dashboard/home/skeleton';
+import { siteConfig } from '@/config/site';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -13,9 +13,13 @@ const DashboardPage = async () => {
   const data = await getDashboardData();
 
   return (
-    <Suspense fallback={<DashboardSkeleton />}>
+    <div className="space-y-8">
+      <Heading
+        title="Bienvenido de nuevo"
+        subtitle={`Conozca el estado del sistema de votación electrónica ${siteConfig.name}`}
+      />
       <DashboardClient data={data} />
-    </Suspense>
+    </div>
   );
 };
 
