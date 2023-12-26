fa9404b1209f77ce8767fbf338af08b4f2dfcb88
Victor Bayas <victorsbayas@gmail.com>
2023-12-26 15:46:19 -0500

    fix footer

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
 import { Icons } from '@/components/shared/icons';
+import { siteConfig } from '@/config/site';
 
-export function SiteFooter() {
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <footer className="container flex flex-col items-center justify-between gap-4 py-6 md:flex-row">
+    <footer
+      className={cn(
+        'container flex flex-col items-center justify-between gap-4 py-6 md:flex-row',
+        className,
+      )}
+    >
       <div className="flex flex-col items-center gap-2 md:flex-row">
         <Icons.logo />
         <p className="text-center text-sm leading-loose md:text-left">
