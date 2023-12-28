0bdbc29c12f11d072edc524aa04ee4b1c04dd46a
Victor Bayas <victorsbayas@gmail.com>
2023-12-28 17:33:06 -0500

    add logging to prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var, no-unused-vars
-  var cachedPrisma: PrismaClient;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export let prisma: PrismaClient;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
